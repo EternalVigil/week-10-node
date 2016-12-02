0b9cbe3675e3d69d59d958b007902316208bc224
Brandon <theeternalvigil@gmail.com>
2016-12-01 18:31:41 -0800

    initial commit

---
+++ javascript/keys.js
@@ -0,0 +1,8 @@
+console.log("This is loaded.");
+
+exports.twitterKeys = {
+	consumer_key: "Ao1JExsykKHEYUZrq6VgG2ByJ",
+	consumer_secret: "x9suSOchBalUb3dogRINN1IJ6FFRZ78UEK4fGUBNtcJdJ382ZN",
+	access_token_key: "804503969498136576-K15qNNMxzovXd8lc1dUeJxPA1ZQzeqL",
+	access_token_secret: "AXykWP5peIjDUasIgaxzcbozGIOnouvYb7XlPMPKSjbaY"
+};
